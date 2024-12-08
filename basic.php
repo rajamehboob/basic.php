34710b27b891f6b2dfffd2c265a3c57a058a3af9
rajamehboob <mahboobrehman@ymail.com>
2024-12-08 19:09:38 +0500

    Create basic.php

---
+++ basic.php
@@ -0,0 +1,11 @@
+<?php
+// Declare variables
+$name = "Mehboob ur rehman";          // String data type
+$age = 44;                   // Integer data type
+$favorite_color = "Green";    // String data type
+
+// Display the variables
+echo "Name: " . $name . "<br>";
+echo "Age: " . $age . "<br>";
+echo "Favorite Color: " . $favorite_color . "<br>";
+?>
